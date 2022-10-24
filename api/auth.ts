47b0ef68a660dfcf08c933b7ca1df9611b69c62d
tony <sumonroytony@gmail.com>
2022-10-24 23:28:20 +0600

    axios added

---
+++ api/auth.ts
@@ -0,0 +1,5 @@
+// import axios from "axios";
+
+// export const login = (payload: LoginPayload) => {
+//     return axios.post('/api/login', payload);
+// }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+  type DehydratedState,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
